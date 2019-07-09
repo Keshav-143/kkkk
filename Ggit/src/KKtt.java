0c6aeefab03b9b339e92e4dee46137563dbee25a
Keshav-143 <keshav.kumar173@gmail.com>
2019-07-09 21:06:47 +0530

    Overwridden 2nd

--- Ggit/src/KKtt.java
+++ Ggit/src/KKtt.java
@@ -4,7 +4,8 @@ public class KKtt {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         System.out.println("kkkjhhdjh");
-        System.out.println("pppppppppppppppppp");
+        System.out.println("pppppppppppppppp");
+        System.out.println("Overwriden");
 	}
 
 }
